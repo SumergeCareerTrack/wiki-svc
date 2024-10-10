53e37b4d63d037fda97277b335cbbf818f6fa700
Youssef Fahmy <youssef.tfs2@gmail.com>
2024-10-10 13:18:01 +0300

    Initial Commit

---
+++ src/main/java/com/sumerge/careertrack/wiki_svc/WikiSvcApplication.java
@@ -0,0 +1,13 @@
+package com.sumerge.careertrack.wiki_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sumerge/careertrack/wiki_svc/WikiSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumerge.careertrack.wiki_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
